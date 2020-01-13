e8d8652e0ca9cd3834b5aa623091a3620c077c71
Isabelle Zheng <isa.zheng@gmail.com>
2020-01-13 11:30:38 -0800

    Make login view work

--- Airpay/LoginViewController.swift
+++ Airpay/LoginViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    
+    var user: User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        continueButton.isEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateContinueButtonState()
+        navigationItem.title = textField.text
+    }
+    
+    
+    // MARK: Private Methods
+    private func updateContinueButtonState() {
+        let text = nameTextField.text ?? ""
+        continueButton.isEnabled = !text.isEmpty
+    }
 
 }
 
