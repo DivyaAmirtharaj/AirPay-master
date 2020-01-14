8961dea568bd61b2ed882dbb2892072209080fad
Isabelle Zheng <isa.zheng@gmail.com>
2020-01-13 19:18:23 -0800

    Combine multipeer stuff

--- Airpay/PaymentViewController.swift
+++ Airpay/PaymentViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import MultiPeer
+
+enum DataType: UInt32 {
+    case message = 1
+    case image = 2
+}
+
+
 
 class PaymentViewController: UIViewController {
     
@@ -14,6 +22,9 @@ class PaymentViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var requestButton: UIButton!
+    @IBOutlet weak var payButton: UIButton!
     
     var user: User?
     
@@ -33,18 +44,114 @@ class PaymentViewController: UIViewController {
             balanceLabel.text = String(balanceText)
         }
         
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+
+        MultiPeer.instance.delegate = self
+
+        MultiPeer.instance.initialize(serviceType: "sample-app")
+        MultiPeer.instance.autoConnect()
+        
     }
     
+    
+    // Dismiss keyboard on tap
+      override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          self.view.endEditing(true)
+      }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        MultiPeer.instance.disconnect()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        super.viewWillDisappear(animated)
     }
-    */
+    
+    
+    // MARK: Actions
+    
+    @IBAction func didPressLoadButton(_ sender: Any) {
 
-}
+        }
+
+    @IBAction func didPressRequestButton(_ sender: Any) {
+        
+        // Device will stop advertising/browsing until after MultiPeer has sent data
+        MultiPeer.instance.stopSearching()
+        
+        defer {
+            MultiPeer.instance.autoConnect()
+            }
+        
+        if let message = textField.text {
+            MultiPeer.instance.send(object: "-$" + message, type: DataType.message.rawValue)
+            }
+        
+    }
+    
+    @IBAction func didPressPayButton(_ sender: Any) {
+        
+        // Device will stop advertising/browsing until after MultiPeer has sent data
+        MultiPeer.instance.stopSearching()
+        
+        defer {
+            MultiPeer.instance.autoConnect()
+            }
+        
+        if let message = textField.text {
+            MultiPeer.instance.send(object: "+$" + message, type: DataType.message.rawValue)
+            }
+        }
+    }
+    
+    
+    extension PaymentViewController: MultiPeerDelegate {
+
+        func multiPeer(didReceiveData data: Data, ofType type: UInt32) {
+            switch type {
+            case DataType.message.rawValue:
+                guard let message = data.convert() as? String else { return }
+                textField.text = message
+                break
+            
+            default:
+                break
+            }
+        }
+
+        func multiPeer(connectedDevicesChanged devices: [String]) {
+            print("Connected devices changed: \(devices)")
+        }
+    }
+
+    extension PaymentViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // Local variable inserted by Swift 4.2 migrator.
+    let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+            
+            if let pickedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
+               // imageView.image = pickedImage
+            }
+            
+            dismiss(animated: true, completion: nil)
+        }
+
+    }
+
+    extension PaymentViewController: UITextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
+    }
+
+    // Helper function inserted by Swift 4.2 migrator.
+    fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+        return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+    }
+
+    // Helper function inserted by Swift 4.2 migrator.
+    fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+        return input.rawValue
+    }
+
